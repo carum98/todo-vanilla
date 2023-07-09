339f3c3f699bcdad72710186238c58910a81e80c
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-07-09 14:04:49 -0600

    Style login and register page

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,33 @@
+window.addEventListener('DOMContentLoaded', init, { once: true })
+
+function init() {
+    const form = document.querySelector('form')
+
+    if (!(form instanceof HTMLFormElement)) {
+        throw new Error('Error to find form')
+    }
+
+    // Disable submit button if any input is empty
+    form.addEventListener('input', () => {
+        const submit = form.querySelector('input[type="submit"]')
+        const inputs = form.querySelectorAll('input:not([type="submit"])')
+
+        if (!(submit instanceof HTMLInputElement)) {
+            throw new Error('Error to find submit or inputs')
+        }
+        
+        const empty = Array.from(inputs).filter(input => input instanceof HTMLInputElement ? input.value === '' : false)
+
+        submit.disabled = empty.length > 0
+    })
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        auth(form, form.getAttribute('action'))
+    }, { once: true })
+}
+
 function auth(form, path) {
     const formData = new FormData(form)
     const data = Object.fromEntries(formData)
@@ -9,7 +39,17 @@ function auth(form, path) {
         },
         body: JSON.stringify(data)
     })
-    .then(response => response.json())
+    .then(response => {
+        if (response.status === 404) {
+            throw new Error('Not found')
+        }
+
+        if (response.status === 500) {
+            throw new Error('Internal server error')
+        }
+
+        return response.json()
+    })
     .then(data => {
         window.location.href = 'index.html'
         sessionStorage.setItem('token', data.token)
